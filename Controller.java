a7a38a151d099a15176dfa917d64434820a9fcc4
BanachObama <BanachObama@gmail.com>
2013-12-19 00:25:13 +0100

    Create Controller.java

---
+++ Controller.java
@@ -0,0 +1,69 @@
+package labb5;
+
+import javax.swing.*;
+import java.net.*;
+import javax.swing.event.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+
+public class Controller {
+
+    private String webAdress;
+    private View view;
+    private URL url;
+
+    public static void main(String[] args) {
+        new Controller(new View());
+    }
+
+    public Controller(final View view) {
+        this.view = view;
+        view.getTextField().addActionListener(new TextFieldListener());
+        view.getBackButton().addActionListener(new BackButtonListener());
+        view.getForwardButton().addActionListener(new ForwardButtonListener());
+        view.getEditorPane().addHyperlinkListener(new HyperlinkListener() {
+
+            public void hyperlinkUpdate(HyperlinkEvent e) {
+                if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+                    try {
+                        view.getEditorPane().setPage(e.getURL());
+                    } catch (IOException ex) {
+                        System.out.println("Kunde inte öppna URL: " + e.getURL());
+                    }
+                }
+            }
+        });
+    }
+
+    public class TextFieldListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            JTextField field = (JTextField) e.getSource();
+            webAdress = field.getText();
+            try {
+                url = new URL(webAdress);
+                if (url != null) {
+                    view.getEditorPane().setPage(url);
+                }
+            } catch (IOException ex) {
+                System.out.println("Det gick inte att läsa URL: " + url);
+            }
+
+        }
+    }
+
+    public class BackButtonListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            BackForwardButton button = (BackForwardButton) e.getSource();
+        }
+    }
+
+    public class ForwardButtonListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            BackForwardButton button = (BackForwardButton) e.getSource();
+        }
+    }
+}
