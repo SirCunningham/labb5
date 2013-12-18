9a521f06c62de2eb171d04a9fbd2fd42b3ff4eaa
BanachObama <BanachObama@gmail.com>
2013-12-19 00:25:49 +0100

    Create BackForwardButton.java

---
+++ BackForwardButton.java
@@ -0,0 +1,14 @@
+package labb5;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.*;
+
+public class BackForwardButton extends JButton{
+    private String name;
+    
+    public BackForwardButton(String name) {
+        this.name=name;
+        setText(name);
+    }
+}
