43f4b4fd552bd4f6fa926c91b6ff4e85010ade57
SirCunningham <lvckv_v@yahoo.se>
2013-12-20 03:04:25 +0100

    Update BackForwardButton.java

--- BackForwardButton.java
+++ BackForwardButton.java
@@ -1,7 +1,6 @@
 package labb5;
 
 import javax.swing.*;
-import java.awt.image.*;
 import java.io.*;
 import javax.imageio.ImageIO;
 
@@ -11,7 +10,7 @@ public class BackForwardButton extends JButton {
         try {
             setIcon(new ImageIcon(ImageIO.read(new File(pathToIcon))));
         } catch (IOException e) {
-            System.err.println("Filen: " + pathToIcon + " kunde inte hittas.");
+            System.err.println("Följande fil kunde inte hittas: " + pathToIcon);
             e.printStackTrace();
         }
     }
