23176e578ef0c02905625cb28dec747f1ebf85e3
SirCunningham <lvckv_v@yahoo.se>
2013-12-20 03:23:01 +0100

    Update HistoryButton.java

--- HistoryButton.java
+++ HistoryButton.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class HistoryButton extends JButton {
 
     public HistoryButton(String text) {
-        setFont(this.getFont().deriveFont(13.0f));
+        setFont(getFont().deriveFont(13.0f));
         setText(text);
     }
 
